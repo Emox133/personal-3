1bea0705bdcffe8075b02322a1d1d4e4adcaa1fe
Emox133 <emir.betaacc0unt@gmail.com>
2021-02-04 20:01:10 +0100

    fixed home route

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
   ) : 
   (
     <Switch>
-      <Route exact path="/" component={Home}/>
       <Route path="/signup" component={Signup}/>
       <Route path="/login" component={Login}/>
     </Switch>)

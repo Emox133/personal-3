6f3261ada4cc3c4954202fecca3dd6623efc8004
Emox133 <emir.betaacc0unt@gmail.com>
2021-04-06 09:48:21 +0200

    fixing image upload

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import HomeMaterial from './pages/HomeMaterial'
 import MyAdvertisements from './pages/MyAdvertisements'
 import NoConnection from './components/layout/NoConnection'
 
-axios.defaults.baseURL = 'https://glacial-lake-62293.herokuapp.com/api/v1'
+// axios.defaults.baseURL = 'https://glacial-lake-62293.herokuapp.com/api/v1'
  
 function App() {
   const {auth, handleLogout, setAuth, fetchUser} = useAuth()
--- src/components/ModalMaterial.js
+++ src/components/ModalMaterial.js
@@ -50,10 +50,11 @@ export default function FormDialog({isOpen, close}) {
 
   const handleImage = e => {
     image = e.target.files[0]
-    setFields({
-      ...fields,
-      photo: image
-    })
+    // setFields({
+    //   ...fields,
+    //   photo: image
+    // })
+    formData.append('photo', image)
   };
 
   const handleInputChange = (e) => {
@@ -74,7 +75,7 @@ export default function FormDialog({isOpen, close}) {
     .replace(/[\u0300-\u036f]/g, "")
     .replaceAll('Đ', 'Dj')
     .replaceAll('đ', 'dj'));
-    formData.append('photo', fields.photo); 
+    // formData.append('photo', fields.photo); 
     formData.append('companyName', fields.companyName)
     formData.append('companyEmail', fields.companyEmail)
     formData.append('companyNumber', fields.companyNumber)
